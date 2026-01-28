f4a5c4fcf21d575e394ae88c62e43945cbd9d998
svenkae1234 <sven.kaehler@freenet.de>
2026-01-28 19:54:32 +0100

    Fixed PythonExpression quoting in simulator condition

--- duatic_helpers/launch_helper.py
+++ duatic_helpers/launch_helper.py
@@ -4,10 +4,7 @@ from launch.substitutions import LaunchConfiguration, PythonExpression
 def simulator_not_subcomponent_condition(simulator: str):
     return IfCondition(
         PythonExpression([
-            "'",
-            LaunchConfiguration("start_as_subcomponent"),
-            "'.lower() != 'true' and '",
-            LaunchConfiguration("simulator"),
-            f"' == '{simulator}'",
+            "('", LaunchConfiguration("start_as_subcomponent"), "'.lower() != 'true') and ",
+            "('", LaunchConfiguration("simulator"), "' == '", simulator, "')",
         ])
     )
